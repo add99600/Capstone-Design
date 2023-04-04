55527ec5bab5d16370758b3a13fb0bf83c67b1cd
hjlee <dlghdwo5153@gmail.com>
2023-04-04 17:22:51 +0900

    스크립트코드를 js파일로 전환하려다 실패

---
+++ templates/v2023/freeboard.js
@@ -0,0 +1,47 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCunQjLi23bBAybA97IFMaIrXUwuJSV8mM",
+    authDomain: "wepdo-7bed1.firebaseapp.com",
+    databaseURL: "https://wepdo-7bed1-default-rtdb.firebaseio.com",
+    projectId: "wepdo-7bed1",
+    storageBucket: "wepdo-7bed1.appspot.com",
+    messagingSenderId: "204948006692",
+    appId: "1:204948006692:web:bbb863d7032ddbbf64b5b1",
+    measurementId: "G-7NQSDSFC61"
+};
+firebase.initializeApp(firebaseConfig);
+
+
+const db = firebase.firestore(); // db문법 소환
+db.collection('product').get().then((결과)=>{
+    결과.forEach((doc)=>{
+        console.log(doc.data());
+
+        var list = `    <div>
+                           <div class="num">1</div>
+                           <div class="title"><a href="http://127.0.0.1:8000/view">${doc.data().제목}</a></div>
+                           <div class="writer">ㅎㅇ</div>
+                           <div class="date">날짜</div>
+                           <div class="count">${doc.data().조회수}</div>
+                           </div>`;
+        $('.board_list').append(list);
+
+        // 아래코드들은 조회수 +1 하고 해당 링크로 이동시키는 코드
+        $('.board_list').on('click', '.title a', function(event) {
+            event.preventDefault(); // 기본 동작 취소
+
+            const postId = $(this).closest('div').find('.count'); // 게시글의 count 요소
+
+            // 해당 게시글의 '조회수' 필드를 1 증가시킴
+            db.collection('product').doc(doc.id).update({
+                조회수: firebase.firestore.FieldValue.increment(1)
+            })
+                .then(() => {
+                    postId.text(parseInt(postId.text()) + 1); // count 값을 1 증가시킴
+                    window.location.href = $(this).attr('href');
+                })
+                .catch((error) => {
+                    console.error('Error updating document: ', error);
+                });
+        });
+    });
+});
---
+++ templates/v2023/freeboard1.js
@@ -0,0 +1,16 @@
+// Firebase SDK 로드
+const script2 = document.createElement('script');
+script2.src = 'https://www.gstatic.com/firebasejs/8.6.5/firebase-app.js';
+document.head.appendChild(script2);
+
+const script3 = document.createElement('script');
+script3.src = 'https://www.gstatic.com/firebasejs/8.6.5/firebase-auth.js';
+document.head.appendChild(script3);
+
+const script4 = document.createElement('script');
+script4.src = 'https://www.gstatic.com/firebasejs/8.6.5/firebase-firestore.js';
+document.head.appendChild(script4);
+
+const script5 = document.createElement('script');
+script5.src = 'https://www.gstatic.com/firebasejs/8.6.5/firebase-storage.js';
+document.head.appendChild(script5);
