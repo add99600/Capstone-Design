d2f397a1f8d914724162644846731abdd02d4e28
hjlee <dlghdwo5153@gmail.com>
2023-05-05 21:00:48 +0900

    마켓 뷰 코드 간결화

--- static/js/freeboard.js
+++ static/js/freeboard.js
@@ -17,13 +17,13 @@ db.collection('product').orderBy('date','desc').limit(5).get().then((결과) => 
         결과.forEach((doc)=>{
             console.log(doc.data());
 
-        var list = `<div>
-                        <div class="num">1</div>
-                        <div class="title"><a href="http://127.0.0.1:8000/fb_view.html?id=${doc.id}">${doc.data().제목}</a></div>
-                        <div class="writer">작성자</div>
-                        <div class="date">${doc.data().날짜}</div>
-                        <div class="count">조회수</div>
-                    </div>`;
-        $('.board_list').append(list);
+            var list = `<div>
+                            <div class="num">1</div>
+                            <div class="title"><a href="http://127.0.0.1:8000/fb_view.html?id=${doc.id}">${doc.data().제목}</a></div>
+                            <div class="writer">작성자</div>
+                            <div class="date">${doc.data().날짜}</div>
+                            <div class="count">조회수</div>
+                        </div>`;
+            $('.board_list').append(list);
     });
 });
