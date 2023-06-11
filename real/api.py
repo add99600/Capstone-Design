44329824625a6627fe2e50ed5575233702038cd1
hjlee <dlghdwo5153@gmail.com>
2023-06-11 18:22:46 +0900

    계산값이 잘못 나오던 현상 수정

--- real/api.py
+++ real/api.py
@@ -5,16 +5,15 @@ from datetime import datetime, timedelta
 import time
 
 def check(search_date, item):
-    start = time.time()
     api_key = '399a12818bb184fe2a3f0d3c393c10da8f967c234392c801455bb606d426ce6c'
-    tday = datetime.today() - timedelta(1)
-    tday = int(tday.strftime("%Y%m%d"))
-
     url = f'http://211.237.50.150:7080/openapi/{api_key}/xml/Grid_20141119000000000012_1/1/1000?AUCNG_DE={search_date}&PRDLST_NM={item}'
     content = requests.get(url).content
-    dict = xmltodict.parse(content)
+    data_dict = xmltodict.parse(content)
 
-    totalCnt = int(dict['Grid_20141119000000000012_1']['totalCnt'])
+    totalCnt = int(data_dict['Grid_20141119000000000012_1']['totalCnt'])
+
+    if totalCnt == 0:
+        return 0
 
     page_size = 1000
     num_pages = (totalCnt + page_size - 1) // page_size
@@ -31,9 +30,9 @@ def check(search_date, item):
             total_price.append(int(row['AVRG_AMT']))
 
     avg_total_price = sum(total_price) // totalCnt
-    end = time.time()
-    runtime = end - start
     return avg_total_price
+
+
 
 
 def check_price(request):
@@ -43,6 +42,7 @@ def check_price(request):
     context = {
         'search_date': search_date,
         'item': item,
-        'avg_total_price': avg_total_price
+        'avg_total_price': avg_total_price,
     }
-    return render(request, '../public/v2023/home.html', context)
+    return render(request, 'v2023/home.html', context)
+
