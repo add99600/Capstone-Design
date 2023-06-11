4561defaf30206e63b2a44558ba6e36ebfecc13a
hjlee <dlghdwo5153@gmail.com>
2023-06-11 17:49:31 +0900

    자유게시판 페이징 처리 완료

--- static/js/freeboard.js
+++ static/js/freeboard.js
@@ -13,18 +13,20 @@ firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
 
-// 초기 데이터 로드
+var firstVisible;
 var lastVisible;
 
 // 첫 페이지 데이터 로드
-db.collection('product').orderBy('date', 'desc').limit(5).get().then((documentSnapshots) => {
-  documentSnapshots.forEach((doc) => {
-    lastVisible = doc;
-    console.log(doc.data());
+db.collection('product').orderBy('num', 'desc').limit(5).get().then((documentSnapshots) => {
+  const docs = documentSnapshots.docs;
+  firstVisible = docs[0];
+  lastVisible = docs[docs.length - 1];
 
+  docs.forEach((doc) => {
+    console.log(doc.data());
     var list = `<div>
                   <div class="num">1</div>
-                  <div class="title" ><a style="text-decoration-line: none; color: black" href="http://127.0.0.1:8000/fb_view.html?id=${doc.id}">${doc.data().제목}</a></div>
+                  <div class="title"><a style="text-decoration-line: none; color: black" href="http://127.0.0.1:8000/fb_view.html?id=${doc.id}">${doc.data().제목}</a></div>
                   <div class="writer">${doc.data().이름}</div>
                   <div class="date">${doc.data().날짜}</div>
                   <div class="count">조회수</div>
@@ -33,17 +35,22 @@ db.collection('product').orderBy('date', 'desc').limit(5).get().then((documentSn
   });
 
   // "다음" 버튼 클릭 시 실행될 함수
-    $('#next').on('click', function() {
-      if (lastVisible === undefined) {
-        return; // 마지막 페이지인 경우, 다음 페이지를 불러오지 않음
-      }
+  $('#next').on('click', function() {
+    if (lastVisible === undefined) {
+      return; // 마지막 페이지인 경우, 다음 페이지를 불러오지 않음
+    }
 
-      $('.board_list').children().not('#top').remove();
+    $('.board_list').children().not('#top').remove();
 
-      const next = db.collection("product").orderBy('date', 'desc').startAfter(lastVisible).limit(5);
+    const next = db.collection("product").orderBy('num', 'desc').startAfter(lastVisible.data().num).limit(5);
 
-      next.get().then((result) => {
-        result.forEach((doc) => {
+    next.get().then((result) => {
+      const nextDocs = result.docs;
+      if (nextDocs.length > 0) {
+        firstVisible = nextDocs[0];
+        lastVisible = nextDocs[nextDocs.length - 1];
+
+        nextDocs.forEach((doc) => {
           console.log(doc.data());
           var list = `<div>
                         <div class="num">1</div>
@@ -52,24 +59,42 @@ db.collection('product').orderBy('date', 'desc').limit(5).get().then((documentSn
                         <div class="date">${doc.data().날짜}</div>
                         <div class="count">조회수</div>
                       </div>`;
-          $('#board').append(list);
-          lastVisible = doc;
+          $('.board_list').append(list);
         });
-      });
+      }
     });
-});
+  });
 
-
-
-
-$('.on').click(function(){
-    const my_uid = JSON.parse(localStorage.getItem('user')).uid
-
-    if (my_uid) {
-        window.location.href = 'http://127.0.0.1:8000/fb_write.html?id=' + my_uid;
-    } else {
-        // 로그인이 되어 있지 않은 경우, 로그인 페이지로 이동하도록 설정할 수 있습니다.
-        window.location.href = 'http://127.0.0.1:8000/login.html';
+  // "이전" 버튼 클릭 시 실행될 함수
+  $('#prev').on('click', function() {
+    if (firstVisible === undefined) {
+      return; // 첫 페이지인 경우, 이전 페이지를 불러오지 않음
     }
+
+    $('.board_list').children().not('#top').remove();
+
+    const prev = db.collection("product").orderBy('num', 'desc').endBefore(firstVisible.data().num).limitToLast(5);
+
+    prev.get().then((result) => {
+      const prevDocs = result.docs;
+      if (prevDocs.length > 0) {
+        firstVisible = prevDocs[0];
+        lastVisible = prevDocs[prevDocs.length - 1];
+
+        prevDocs.forEach((doc) => {
+          console.log(doc.data());
+          var list = `<div>
+                        <div class="num">1</div>
+                        <div class="title"><a style="text-decoration-line: none; color: black" href="http://127.0.0.1:8000/fb_view.html?id=${doc.id}">${doc.data().제목}</a></div>
+                        <div class="writer">${doc.data().이름}</div>
+                        <div class="date">${doc.data().날짜}</div>
+                        <div class="count">조회수</div>
+                      </div>`;
+          $('.board_list').append(list);
+        });
+      }
+    });
+  });
 });
+
 
